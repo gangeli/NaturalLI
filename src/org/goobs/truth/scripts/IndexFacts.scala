53af42fe6d8cad21d44159833b95dae8285b46eb
Gabor Angeli <angeli@cs.stanford.edu>
2013-10-01 00:23:04 -0700

    I'm not very good at fixing silly bugs...

--- src/org/goobs/truth/scripts/IndexFacts.scala
+++ src/org/goobs/truth/scripts/IndexFacts.scala
@@ -261,7 +261,7 @@ object IndexFacts {
           psql.setAutoCommit(true)
           val factInsert = psql.prepareStatement(
             "INSERT INTO " + Postgres.TABLE_FACTS +
-            " (weight, left_arg, leftHead, rel, right_arg, rightHead) VALUES (?, ?, ?, ?);")
+            " (weight, left_arg, leftHead, rel, right_arg, rightHead) VALUES (?, ?, ?, ?, ?, ?);")
           val factUpdate = psql.prepareStatement(
             "UPDATE " + Postgres.TABLE_FACTS +
             " SET weight=? WHERE left_arg=? AND rel=? AND right_arg=?;")
