5ea33a3e6aa393075264b3a438d39e93d0952215
Gabor Angeli <angeli@cs.stanford.edu>
2014-01-22 23:21:35 -0800

    Beginnings of server code

--- src/InferenceServer.cc
+++ src/InferenceServer.cc
@@ -1,37 +1,102 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include <netdb.h>
-
-#include <sys/wait.h>
-#include <sys/types.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <thread>
 #include <sys/socket.h>
-
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/errno.h>
 #include <arpa/inet.h>
 
-#include <netinet/in.h>
 
-#include "Config.h"
-#include "Utils.h"
-#include "Graph.h"
-#include "FactDB.h"
-#include "Search.h"
-#include "RamCloudBackend.h"
+#ifndef SERVER_PORT
+#define SERVER_PORT       1337
+#define SERVER_TCP_BUFFER 25
+#endif
 
-using namespace std;
+#ifndef ERESTART
+#define ERESTART EINTR
+#endif
+extern int errno;
 
-//
-// ENTRY
-//
+/**
+ * Handle an incoming connection.
+ * This involves reading a query, starting a new inference, and then
+ * closing the connection.
+ */
+void handleConnection(uint32_t socket, sockaddr_in address) {
+  // Read the query
+  printf("[%d] reading query...\n", socket);
+  // Close the connection
+  shutdown(socket, 2);
+  printf("[%d] CONNECTION CLOSED: %s port %d\n", socket,
+		     inet_ntoa(address.sin_addr),
+         ntohs(address.sin_port));
+}
 
-#define PORT "3490"  // the port users will be connecting to
+/**
+ * Set up listening on a server port
+ */
+int startServer(int port) {
+  // Create a socket
+  int32_t sock = socket(AF_INET, SOCK_STREAM, 0);
+  // Bind the socket
+  // (setup the bind)
+  struct sockaddr_in address;
+  memset( (char*) &address, 0, sizeof(address) );  // zero the socket
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = htonl(INADDR_ANY);
+  address.sin_port = htons(port);
+  // (actually perform the bind)
+  if (bind(sock, (struct sockaddr*) &address, sizeof(address)) != 0) {
+  	perror("Could not bind socket!");
+    return 1;
+  } else {
+    printf("Opened server socket.\n");
+  }
 
-#define BACKLOG 10     // how many pending connections queue will hold
+  // set the socket for listening (queue backlog of 5)
+	if (listen(sock, SERVER_TCP_BUFFER) < 0) {
+		perror("Could not open port for listening!");
+    return 2;
+	} else {
+    printf("Listening on port %d...\n", port);
+  }
 
-int main(void)
-{
-    return 0;
+  // Allow immediate re-use of the port
+  int32_t sockoptval = 1;
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &sockoptval, sizeof(int32_t));
+
+  // loop, accepting connection requests
+	for (;;) {
+    // Accept an incoming connection
+    int32_t requestSocket;
+    socklen_t requestLength;
+		while ((requestSocket = accept(sock, (struct sockaddr*) &address, &requestLength)) < 0) {
+			// we may break out of accept if the system call
+      // was interrupted. In this case, loop back and
+      // try again
+      if ((errno != ECHILD) && (errno != ERESTART) && (errno != EINTR)) {
+        // If we got here, there was a serious problem with the connection
+        perror("Failed to accept connection request");
+        return 3;
+      } 
+    }
+		// Connection established
+    printf("[%d] CONNECTION ESTABLISHED: %s port %d\n", requestSocket,
+			     inet_ntoa(address.sin_addr),
+           ntohs(address.sin_port));
+    std::thread t(handleConnection, requestSocket, address);
+	}
+
+  return 0;
+}
+
+int main( int argc, char *argv[] ) {
+  startServer(argc < 2 ? SERVER_PORT : atoi(argv[1]) );
+//  while (startServer(1337) != 0) { }
+  printf("\n");
+  printf("--------------\n");
+  printf("STOPPED SERVER\n");
+  printf("--------------\n");
 }
