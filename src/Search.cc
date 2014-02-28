5b3255c65f857020dd7aa002aedaf13b396d6bd9
Gabor Angeli <angeli@cs.stanford.edu>
2014-02-27 20:28:02 -0800

    Remove debugging statements

--- src/Search.cc
+++ src/Search.cc
@@ -526,7 +526,7 @@ vector<scored_path> Search(Graph* graph, FactDB* knownFacts,
     cache->add(parent->fact, parent->factLength);
 
     // -- Debug Output --
-    printf("%lu [%f] %s\n", time, costSoFar, toString(*graph, parent->fact, parent->factLength).c_str());
+//    printf("%lu [%f] %s\n", time, costSoFar, toString(*graph, parent->fact, parent->factLength).c_str());
     // Update time
     time += 1;
     if (time % tickTime == 0) {
@@ -564,7 +564,6 @@ vector<scored_path> Search(Graph* graph, FactDB* knownFacts,
     for (uint8_t indexToMutate = 0;
          indexToMutate < parentLength;
          ++indexToMutate) {  // for each index to mutate...
-      printf("  mutating index %d\n", indexToMutate);
       if (isSetBit(fixedBitmask, indexToMutate)) { continue; }
       uint32_t numMutations = 0;
       const tagged_word parentWord = parentFact[indexToMutate];
@@ -574,7 +573,6 @@ vector<scored_path> Search(Graph* graph, FactDB* knownFacts,
       for (int i = 0; i < numMutations; ++i) {
         const edge& mutation = mutations[i];
         if (mutation.sense != parentSense) {
-          printf("    bad sense: %d but wanted %d\n", mutation.sense, parentSense);
           continue;
         }
         // Flush if necessary (save memory)
@@ -598,7 +596,6 @@ vector<scored_path> Search(Graph* graph, FactDB* knownFacts,
           typeArr[queueLength] = mutation.type;
           costArr[queueLength] = costSoFar + mutationCost;
           queueLength += 1;
-          printf("    pushing!\n");
         }
       }
     }
