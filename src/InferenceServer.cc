b3957a025b77c651f7d9b1a1fe278a3b5b3dfa70
Gabor Angeli <angeli@cs.stanford.edu>
2014-03-13 23:16:25 -0700

    Revert to usual main() method

--- src/InferenceServer.cc
+++ src/InferenceServer.cc
@@ -415,6 +415,5 @@ int startServer(int port) {
  * The server's entry point.
  */
 int main( int argc, char *argv[] ) {
-//  while (startServer(argc < 2 ? SERVER_PORT : atoi(argv[1]))) { usleep(1000000); }
-  ReadFactTrie();
+  while (startServer(argc < 2 ? SERVER_PORT : atoi(argv[1]))) { usleep(1000000); }
 }
