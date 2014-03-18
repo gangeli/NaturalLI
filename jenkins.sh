d839230e04de6902488b130375b911e43e5cb107
Gabor Angeli <angeli@cs.stanford.edu>
2014-03-17 22:56:49 -0700

    Consolidate the Jenkins jobs into a single script

---
+++ jenkins.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+
+set -e
+
+echo "-- SETUP --"
+rm lib/stanford-corenlp-*
+ln -s $HOME/stanford-corenlp-* lib/
+
+echo "-- MAKE --"
+make clean
+make all
+
+echo "-- START SERVER --"
+dist/server &
+
+echo "-- TESTS --"
+valgrind --leak-check=yes dist/test_server
+sleep 120
+make test
+
+echo "-- SHUTDOWN SERVER --"
+java -cp $JAVANLP_HOME/projects/core/classes:lib/protobuf.jar:dist/test_client.jar org.goobs.truth.scripts.ShutdownServer
+
+echo "SUCCESS!"
