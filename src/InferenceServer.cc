8203bc3a9dc9975bdffaa606161d4d08058db2d7
Gabor Angeli <angeli@cs.stanford.edu>
2014-05-04 19:41:15 -0700

    Err... make the program do something again :|

--- src/InferenceServer.cc
+++ src/InferenceServer.cc
@@ -433,6 +433,5 @@ int startServer(int port) {
  * The server's entry point.
  */
 int main( int argc, char *argv[] ) {
-  printf("%lu; pointer=%lu\n", sizeof(Trie), sizeof(Trie*));
-//  while (startServer(argc < 2 ? SERVER_PORT : atoi(argv[1]))) { usleep(1000000); }
+  while (startServer(argc < 2 ? SERVER_PORT : atoi(argv[1]))) { usleep(1000000); }
 }
