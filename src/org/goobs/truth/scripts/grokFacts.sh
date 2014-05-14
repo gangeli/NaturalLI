75380f2c5da6a26061eda4ecedf99ff1835f8648
Gabor Angeli <angeli@cs.stanford.edu>
2014-05-13 19:59:55 -0700

    Add script to create head-only fact database

--- src/org/goobs/truth/scripts/grokFacts.sh
+++ src/org/goobs/truth/scripts/grokFacts.sh
@@ -11,6 +11,7 @@ PGDATABASE=truth
 
 OUT_OPENIE_UNNORMALIZED='output/ollie_unnormalized.tab'
 OUT_OPENIE_INDEXED='output/ollie_indexed.tab'
+OUT_OPENIE_INDEXED_HEADS='output/ollie_indexed_heads.tab'
 
 JOBS=6
 
@@ -22,12 +23,14 @@ echo "a rather large number of files taking up a lot of space"
 echo "IN THE CURRENT DIRECTORY."
 echo ""
 echo "Created Files:"
-echo "  $OUT_OPENIE_UNNORMALIZED.bz2: All extractions in a single file"
-echo "  $OUT_OPENIE_INDEXED.bz2       The indexed facts"
+echo "  $OUT_OPENIE_UNNORMALIZED.bz2:  All extractions in a single file"
+echo "  $OUT_OPENIE_INDEXED.bz2:       The indexed facts"
+echo "  $OUT_OPENIE_INDEXED_HEADS.bz2: The indexed facts, with only head words"
 echo ""
 echo "Temporary Files:"
-echo "  $OUT_OPENIE_UNNORMALIZED: All extractions in a single file (unzipped)"
-echo "  $OUT_OPENIE_INDEXED:      The indexed facts (unzipped)"
+echo "  $OUT_OPENIE_UNNORMALIZED:  All extractions in a single file (unzipped)"
+echo "  $OUT_OPENIE_INDEXED:       The indexed facts (unzipped)"
+echo "  $OUT_OPENIE_INDEXED_HEADS: The indexed facts with only head words (unzipped)"
 echo ""
 echo "Input:"
 echo "  $INPUT_DIR: All files in this directory will be used as input"
@@ -53,7 +56,7 @@ trap cleanup EXIT
 # ###########################
 # AGGREGATE FACTS
 # ###########################
-echo "[10] Sort+Uniq Facts ($OUT_OPENIE_UNNORMALIZED)"
+#echo "[10] Sort+Uniq Facts ($OUT_OPENIE_UNNORMALIZED)"
 #echo "$INPUT_DIR" | grep "reverb" > /dev/null
 #if [ "$?" == "0" ]; then
 #  # ReVerb
@@ -113,30 +116,53 @@ cat $WORDS |\
 # ###########################
 # INDEX FACTS
 # ###########################
-echo "[40] Index Facts ($OUT_OPENIE_INDEXED)"
+#echo "[40] Index Facts ($OUT_OPENIE_INDEXED)"
+#bunzip2 $OUT_OPENIE_UNNORMALIZED.bz2 || true
+#cat $OUT_OPENIE_UNNORMALIZED |\
+#  grep -a -P '^[\x01-\x7f]+$' |\
+#  cut -f 2-4 |\
+#  sed -e 's/\t/ /g'  -e 's/\. //g' -e 's/["\(\)]//g' -e 's/[0-9]+//g' |\
+#  ( parallel --jobs $JOBS --gnu --block 100M --pipe ./lemmatize ) |\
+#  ( parallel --jobs $JOBS --gnu --pipe ./fastsed $TMP_SED_CASED )    |\
+#  ( parallel --jobs $JOBS --gnu --pipe ./fastsed $TMP_SED_CASELESS ) |\
+#  sed -e 's/[ 	]+/ /g' |\
+#  paste $OUT_OPENIE_UNNORMALIZED - |\
+#  ( parallel --jobs $JOBS --gnu --pipe cut -f 1,5) |\
+#  egrep '^[0-9]+	[0-9][0-9 ]*$' > $OUT_OPENIE_INDEXED
+
+echo "[45] Index Facts ($OUT_OPENIE_INDEXED_HEADS)"
 bunzip2 $OUT_OPENIE_UNNORMALIZED.bz2 || true
 cat $OUT_OPENIE_UNNORMALIZED |\
   grep -a -P '^[\x01-\x7f]+$' |\
   cut -f 2-4 |\
-  sed -e 's/\t/ /g'  -e 's/\. //g' -e 's/["\(\)]//g' -e 's/[0-9]+//g' |\
-  ( parallel --jobs $JOBS --gnu --block 100M --pipe ./lemmatize ) |\
+  sed -e 's/\. //g' -e 's/["\(\)]//g' -e 's/[0-9]+//g' |\
+  ( parallel --jobs $JOBS --gnu --block 100M --pipe ./simplify ) |\
+  sed -e 's/\t/ /g' |\
   ( parallel --jobs $JOBS --gnu --pipe ./fastsed $TMP_SED_CASED )    |\
   ( parallel --jobs $JOBS --gnu --pipe ./fastsed $TMP_SED_CASELESS ) |\
   sed -e 's/[ 	]+/ /g' |\
   paste $OUT_OPENIE_UNNORMALIZED - |\
   ( parallel --jobs $JOBS --gnu --pipe cut -f 1,5) |\
-  egrep '^[0-9]+	[0-9][0-9 ]*$' > $OUT_OPENIE_INDEXED
+  egrep '^[0-9]+	[0-9][0-9 ]*$' > $OUT_OPENIE_INDEXED_HEADS
 
 # ###########################
 # IMPORT TO PSQL
 # ###########################
-echo "[50] Import to Postgres (from $OUT_OPENIE_INDEXED)"
-/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "DROP TABLE IF EXISTS fact_new;"
-/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "CREATE TABLE fact_new(gloss INTEGER[], weight INTEGER);"
-cat $OUT_OPENIE_INDEXED | grep -v '		' | sed -r -e 's/\s+$//g' -e 's/ +/"",""/g' -e 's/([0-9]+)\t(.*)/"{""\2""}","\1"/g' |\
-  /u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "COPY fact_new FROM STDIN DELIMITER ',' CSV;"
-/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "CREATE INDEX fact_new_weight ON fact_new USING BTREE (weight);"
-/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "CREATE INDEX fact_new_gloss ON fact_new USING GIN (gloss);"
+#echo "[50] Import to Postgres (from $OUT_OPENIE_INDEXED)"
+#/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "DROP TABLE IF EXISTS fact_new;"
+#/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "CREATE TABLE fact_new(gloss INTEGER[], weight INTEGER);"
+#cat $OUT_OPENIE_INDEXED | grep -v '		' | sed -r -e 's/\s+$//g' -e 's/ +/"",""/g' -e 's/([0-9]+)\t(.*)/"{""\2""}","\1"/g' |\
+#  /u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "COPY fact_new FROM STDIN DELIMITER ',' CSV;"
+#/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "CREATE INDEX fact_new_weight ON fact_new USING BTREE (weight);"
+#/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "CREATE INDEX fact_new_gloss ON fact_new USING GIN (gloss);"
+
+echo "[55] Import to Postgres (from $OUT_OPENIE_INDEXED_HEADS)"
+/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "DROP TABLE IF EXISTS fact_heads_new;"
+/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "CREATE TABLE fact_heads_new(gloss INTEGER[], weight INTEGER);"
+cat $OUT_OPENIE_INDEXED_HEADS | grep -v '		' | sed -r -e 's/\s+$//g' -e 's/ +/"",""/g' -e 's/([0-9]+)\t(.*)/"{""\2""}","\1"/g' |\
+  /u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "COPY fact_heads_new FROM STDIN DELIMITER ',' CSV;"
+/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "CREATE INDEX fact_heads_new_weight ON fact_heads_new USING BTREE (weight);"
+/u/nlp/packages/pgsql/bin/psql -U $PGUSER -p $PGPORT -h $PGHOST $PGDATABASE -c "CREATE INDEX fact_heads_new_gloss ON fact_heads_new USING GIN (gloss);"
 
 
 # ###########################
@@ -144,5 +170,7 @@ cat $OUT_OPENIE_INDEXED | grep -v '		' | sed -r -e 's/\s+$//g' -e 's/ +/"",""/g'
 # ###########################
 echo "[60] BZIP $OUT_OPENIE_UNNORMALIZED"
 bzip2 $OUT_OPENIE_UNNORMALIZED
-echo "[70] BZIP $OUT_OPENIE_INDEXED"
+echo "[61] BZIP $OUT_OPENIE_INDEXED"
 bzip2 $OUT_OPENIE_INDEXED
+echo "[62] BZIP $OUT_OPENIE_INDEXED_HEADS"
+bzip2 $OUT_OPENIE_INDEXED_HEADS
