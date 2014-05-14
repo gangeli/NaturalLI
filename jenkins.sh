fd334c3b5687454cc10652c046df0f16efe63011
Gabor Angeli <angeli@cs.stanford.edu>
2014-05-13 23:26:23 -0700

    Don't delete the java test files with a 'make clean'

--- jenkins.sh
+++ jenkins.sh
@@ -29,20 +29,6 @@ make all check TESTS_ENVIRONMENT=true
 echo "-- TEST --"
 test/src/test_server --gtest_output=xml:test/test_server.junit.xml
 test/src/itest_server --gtest_output=xml:test/itest_server.junit.xml
-make java_test
-
-echo "-- COVERAGE --"
-cd src/
-rm naturalli_server-Messages.pb.gcda
-rm naturalli_server-Messages.pb.gcno
-rm -f naturalli_server-Messages.pb.h.gcno
-rm -f naturalli_server-Messages.pb.h.gcda
-gcovr -r . --html --html-details -o /var/www/naturalli/coverage/index.html
-gcovr -r . --xml -o coverage.xml
-cd ..
-
-echo "-- DOCUMENT --"
-doxygen doxygen.conf
 
 echo "--SPECIAL TESTS--"
 echo "(high memory mode)"
@@ -59,6 +45,22 @@ make clean
   --with-scala=$SCALA_HOME \
   --with-java=/usr/lib/jvm/java-7-oracle
 make all check
+
+echo "--JAVA TEST--"
+make java_test
+
+echo "-- COVERAGE --"
+cd src/
+rm naturalli_server-Messages.pb.gcda
+rm naturalli_server-Messages.pb.gcno
+rm -f naturalli_server-Messages.pb.h.gcno
+rm -f naturalli_server-Messages.pb.h.gcda
+gcovr -r . --html --html-details -o /var/www/naturalli/coverage/index.html
+gcovr -r . --xml -o coverage.xml
+cd ..
+
+echo "-- DOCUMENT --"
+doxygen doxygen.conf
 
 echo "SUCCESS!"
 exit 0
