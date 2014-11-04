f84353b6ba45874e4fed6773dc620959662c91d5
Gabor Angeli <angeli@stanford.edu>
2014-11-03 23:52:26 -0800

    Gah; hack to copy words over

--- jenkins.sh
+++ jenkins.sh
@@ -23,6 +23,10 @@ function configure() {
 
 echo "-- CLEAN --"
 git clean -f
+# TODO(gabor) this should probably be removed
+cp /home/gabor/workspace/NaturalLI/etc/words.tab.gz etc/
+cp /home/gabor/workspace/NaturalLI/etc/word_senses.tab.gz etc/
+
 ./autogen.sh
 
 echo "-- MAKE --"
