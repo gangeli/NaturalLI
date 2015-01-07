6dc1459c9152ad335048e082d01d342ee5c7c6ef
Gabor Angeli <angeli@stanford.edu>
2015-01-06 21:59:28 -0800

    Java code to process premises

--- src/edu/stanford/nlp/naturalli/ProcessPremise.java
+++ src/edu/stanford/nlp/naturalli/ProcessPremise.java
@@ -1,9 +1,68 @@
 package edu.stanford.nlp.naturalli;
 
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
+import edu.stanford.nlp.util.CoreMap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
 /**
- * TODO(gabor) JavaDoc
+ * A helper to find the forward entailments for a given input query, and dump all of the entailed
+ * sentences.
  *
  * @author Gabor Angeli
  */
 public class ProcessPremise {
+
+  protected static Collection<SentenceFragment> forwardEntailments(String input, StanfordCoreNLP pipeline) {
+    Annotation ann = new Annotation(input);
+    pipeline.annotate(ann);
+    List<SentenceFragment> entailments = new ArrayList<>();
+    for (CoreMap sentence : ann.get(CoreAnnotations.SentencesAnnotation.class)) {
+      Util.cleanTree(sentence.get(SemanticGraphCoreAnnotations.CollapsedDependenciesAnnotation.class));
+      entailments.add(new SentenceFragment(sentence.get(SemanticGraphCoreAnnotations.CollapsedDependenciesAnnotation.class), false));
+      entailments.addAll(sentence.get(NaturalLogicAnnotations.EntailedSentencesAnnotation.class).stream().collect(Collectors.toList()));
+    }
+    return entailments;
+  }
+
+  protected static StanfordCoreNLP constructPipeline() {
+    Properties props = new Properties() {{
+      setProperty("annotators", "tokenize,ssplit,pos,lemma,depparse,natlog,openie");
+      setProperty("ssplit.isOneSentence", "true");
+      setProperty("tokenize.class", "PTBTokenizer");
+      setProperty("tokenize.language", "en");
+      setProperty("naturalli.doPolarity", "false");
+    }};
+    return new StanfordCoreNLP(props);
+  }
+
+  public static void main(String[] args) throws IOException {
+    // Create pipeline
+    StanfordCoreNLP pipeline = constructPipeline();
+
+    // Read input
+    System.err.println(ProcessPremise.class.getSimpleName() + " is ready for input");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String line;
+    while ((line = reader.readLine()) != null) {
+      if (!line.trim().equals("")) {
+        System.err.println("Annotating '" + line + "'");
+        for (SentenceFragment fragment : forwardEntailments(line.trim(), pipeline)) {
+          System.out.println(ProcessQuery.conllDump(fragment.parseTree));
+          System.out.flush();
+        }
+      }
+    }
+  }
+
 }
