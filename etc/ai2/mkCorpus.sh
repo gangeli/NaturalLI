4a66c1bd007617ab681f4de0c760b2b5059714f1
Gabor Angeli <angeli@stanford.edu>
2015-04-12 23:22:37 -0700

    Tweaks to coref script

--- etc/ai2/mkCorpus.sh
+++ etc/ai2/mkCorpus.sh
@@ -1,48 +1,69 @@
 #!/bin/bash
 #
 
-mv corpus.tab corpus.tab.bak
+set -o errexit
+
+if [ -e corpus.tab ]; then
+  mv corpus.tab corpus.tab.bak
+fi
+touch corpus.tab
+
+export CLASSPATH="$CLASSPATH:/home/gabor/workspace/naturalli/src/naturalli_preprocess.jar"
+export CLASSPATH="$CLASSPATH:/home/gabor/staging/stanford-srparser-models-current.jar"
+
+function generalRead() {
+  cat $1 |\
+    sed -r -e 's/(\.|^)([0-9])([\.\t])/\10\2\3/g' |\
+    sed -r -e 's/\.([0-9])([\.\t])/.0\1\2/g' |\
+    sed -r -e 's/^\t/00\t/g' |\
+    sed -r -e 's/^([0-9]+)\t/\1.00\t/g' |\
+    sed -r -e 's/^([0-9]+\.[0-9]+)\t/\1.00\t/g' |\
+    sed -r -e 's/^([0-9]+\.[0-9]+\.[0-9]+)\t/\1.00\t/g' |\
+    sed -r -e 's/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\t/\1.00\t/g' |\
+    sed -r -e 's/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\t/\1.00\t/g' |\
+    sed -r -e 's/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\t/\1.00\t/g' |\
+    java -mx4g edu.stanford.nlp.naturalli.ExpandCoreference
+}
+
 
 echo "barrons-sentences.txt"
-cat barrons-sentences.txt |\
+generalRead barrons-sentences.txt |\
   awk -F'\t' '{ print "barrons-" $1 "\t" $2 }' \
   >> corpus.tab
 
-echo "c12k-biology-sentences.txt"
-cat ck12-biology-sentences.txt |\
-  awk -F'\t' '{ print "ck12_biology-" $1 "\t" $2 }' \
+echo "ck12-biology-sentences.txt"
+generalRead ck12-biology-sentences.txt |\
+  awk -F'\t' '{ print "c12k_biology-" $1 "\t" $2 }' \
   >> corpus.tab
 
 echo "CurrentWebCorpus-allSources-v1.txt"
-cat CurrentWebCorpus-allSources-v1.txt |\
+generalRead CurrentWebCorpus-allSources-v1.txt |\
   awk -F'\t' '{ print "currentweb-" NR "\t" $1 }' \
   >> corpus.tab
 
 echo "dictionary-sentences.txt"
+TMP=`mktemp`
 cat dictionary-sentences.txt |\
-  awk -F'\t' '{ print "dictionary-" $1 "\t" $2 }' |\
   sed -r \
     -e 's/<term>([^<]+)<\/term>/\1/g' \
     -e 's/<.*:/ is/g' \
-    -e 's/"//g' \
+    -e 's/"//g' > "$TMP"
+echo "Dictionary written to $TMP"
+generalRead "$TMP" |\
+  awk -F'\t' '{ print "dictionary-" $1 "\t" $2 }' \
   >> corpus.tab
+rm "$TMP"
 
 echo "simplewiki-science-sentences.txt"
-export CLASSPATH="$CLASSPATH:/home/gabor/workspace/naturalli/src/naturalli_preprocess.jar"
-export CLASSPATH="$CLASSPATH:/home/gabor/staging/stanford-srparser-models-current.jar"
-cat simplewiki-science-sentences.txt |\
-  sed -r -e 's/\.([0-9])\t/.0\1\t/g' |\
-  sort -n |\
-  java -mx3g edu.stanford.nlp.naturalli.ExpandCoreference |\
-  awk -F'\t' '{ print "simplewiki_science-" $1 "\t" $2 }'
+generalRead simplewiki-science-sentences.txt |\
+  awk -F'\t' '{ print "simplewiki_science-" $1 "\t" $2 }' \
   >> corpus.tab
 
 echo "simplewiki-barrons-sentences.txt"
-cat simplewiki-barrons-sentences.txt |\
-  sed -r -e 's/\.([0-9])\t/.0\1\t/g' |\
-  sort -n |\
-  java -mx3g edu.stanford.nlp.naturalli.ExpandCoreference |\
-  awk -F'\t' '{ print "simplewiki_barrons-" $1 "\t" $2 }'
+generalRead simplewiki-barrons-sentences.txt |\
+  awk -F'\t' '{ print "simplewiki_barrons-" $1 "\t" $2 }' \
   >> corpus.tab
 
-rm corpus.tab.bak
+if [ -e corpus.tab.bak ]; then
+  rm -f corpus.tab.bak
+fi
--- src/edu/stanford/nlp/naturalli/ExpandCoreference.java
+++ src/edu/stanford/nlp/naturalli/ExpandCoreference.java
@@ -1,12 +1,16 @@
 package edu.stanford.nlp.naturalli;
 
-import edu.stanford.nlp.dcoref.CorefChain;
-import edu.stanford.nlp.dcoref.CorefCoreAnnotations;
+import edu.stanford.nlp.ie.NERClassifierCombiner;
+import edu.stanford.nlp.ie.regexp.NumberSequenceClassifier;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.tokensregex.TokenSequenceMatcher;
+import edu.stanford.nlp.ling.tokensregex.TokenSequencePattern;
 import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.DefaultPaths;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.StringUtils;
 import edu.stanford.nlp.util.logging.Redwood;
 
 import java.io.BufferedReader;
@@ -24,21 +28,38 @@ import java.util.regex.Pattern;
  * </p>
  *
  * <p>
- *   This script uses constituency parses (which is very slow). This is mitigated somewhat by using the shift
- *   reduce parser.
+ *   The format of the input to this script is as follows:
  * </p>
+ *
+ * <ul>
+ *   <li>Each line of input should be a sentence. By default, each sentence is a document</li>
+ *   <li>A line can be an id followed by a tab followed by a sentence.</li>
+ *   <li>This id should be seven numbers, separated by '.' or '-'. The first four numbers define the document, the last three
+ *       are a specifier within the document.</li>
+ * </ul>
+ *
+ * <p>
+ *   The script outputs lines with the following (tab separated):
+ * </p>
+ *
+ * <ul>
+ *   <li>The input id, if it was provided, and '?.?.?.?.?.?.?' if it could not be determined.</li>
+ *   <li>The tokenized sentence, with coreference expanded to the best of the script's ability.</li>
+ * </ul>
  *
  * @author Gabor Angeli
  */
 public class ExpandCoreference {
 
   private static class Document {
+    public final String id;
     public final StringBuilder text;
     public final List<String> ids;
 
     public Annotation ann;
 
-    public Document(StringBuilder text, List<String> ids) {
+    public Document(String id, StringBuilder text, List<String> ids) {
+      this.id = id;
       this.text = text;
       this.ids = ids;
     }
@@ -47,11 +68,14 @@ public class ExpandCoreference {
       if (id != null) {
         ids.add(id);
       }
-      this.text.append(text).append("\n");
+      this.text.append(text.replaceAll("\n", "")).append("\n");
     }
   }
 
-  private static Pattern docid = Pattern.compile("([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+)\\.([0-9]+\\.[0-9]+\\.[0-9]+)");
+  private static Pattern docid = Pattern.compile("([0-9]+[\\.\\-][0-9]+[\\.\\-][0-9]+[\\.\\-][0-9]+)[\\.\\-]([0-9]+[\\.\\-][0-9]+[\\.\\-][0-9]+)");
+  private static TokenSequencePattern person = TokenSequencePattern.compile("[{ner:PERSON}]+");
+  private static TokenSequencePattern plural = TokenSequencePattern.compile("([{word:/the|a/}])? [{tag:VBG; word:/[A-Z].*/} || {tag:JJ}]* [{tag:/NNP?S/}]+ ([{lemma:and}] [{tag:JJ}]* [{tag:/NNP?S/}]+)?");
+  private static TokenSequencePattern noun_phrase = TokenSequencePattern.compile("([{word:/the|a/}])? [{tag:VBG; word:/[A-Z].*/} || {tag:JJ}]*  [{tag:/N.*[^S]/}]+");
 
   private static String getDocId(String rawID) {
     Matcher matcher = docid.matcher(rawID);
@@ -62,7 +86,152 @@ public class ExpandCoreference {
     }
   }
 
+  public static void main(String[] args) throws IOException {
+    long startTime = System.currentTimeMillis();
+    StanfordCoreNLP pipeline = new StanfordCoreNLP(new Properties() {{
+      setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
+      setProperty("ssplit.newlineIsSentenceBreak", "always");
+      setProperty("ner.model", DefaultPaths.DEFAULT_NER_THREECLASS_MODEL);
+      setProperty(NERClassifierCombiner.APPLY_NUMERIC_CLASSIFIERS_PROPERTY, "false");
+      setProperty(NumberSequenceClassifier.USE_SUTIME_PROPERTY, "false");
+    }});
+    System.err.println("Done loading CoreNLP: " + Redwood.formatTimeDifference(System.currentTimeMillis() - startTime));
 
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    Document currentDocument = null;
+    String currentDocId = null;
+    List<Document> documents = new ArrayList<>();
+    String line;
+    int linesread = 0;
+    while ( (line = reader.readLine()) != null) {
+      if (line.contains("\t")) {
+        String[] fields = line.split("\t");
+        if (fields.length <= 1 || fields[1].trim().equals("")) {
+          continue;
+        }
+        String docid = getDocId(fields[0]);
+        String text = StringUtils.join(Arrays.asList(fields).subList(1, fields.length), " ");
+        if (docid == null) {
+          throw new IllegalArgumentException("Invalid docid: " + fields[0]);
+        }
+        if (!docid.equals(currentDocId)) {
+          if (currentDocId != null) {
+            documents.add(currentDocument);
+          }
+          currentDocId = docid;
+          currentDocument = new Document(currentDocId, new StringBuilder(), new ArrayList<>());
+        }
+        currentDocument.register(docid, text);
+      } else {
+        currentDocument = new Document(currentDocId, new StringBuilder(), new ArrayList<>());
+        currentDocument.register(null, line);
+        documents.add(currentDocument);
+      }
+      linesread += 1;
+      if (linesread % 10000 == 0) {
+        System.err.println("  Read " + (linesread / 1000) + "k lines (" + (documents.size() + 1) + " documents)");
+      }
+    }
+
+    int numDocsAnnotated = 0;
+    System.err.println("Annotating Documents");
+    for (Document doc : documents) {
+//      System.err.println("-----DOC BOUNDARY: " + doc.id + "-----");
+      doc.ann = new Annotation(doc.text.toString().trim());
+      pipeline.annotate(doc.ann);
+      List<CoreLabel> tokens = doc.ann.get(CoreAnnotations.TokensAnnotation.class);
+      List<CoreMap> sentences = doc.ann.get(CoreAnnotations.SentencesAnnotation.class);
+      Iterator<String> iter = doc.ids.isEmpty() ? null : doc.ids.iterator();
+
+      TokenSequenceMatcher personMatcher = person.matcher(tokens);
+      String firstPerson = personMatcher.find() ? personMatcher.group() : null;
+      TokenSequenceMatcher pluralMatcher = plural.matcher(tokens);
+      String firstPlural = pluralMatcher.find() ? pluralMatcher.group().toLowerCase() : null;
+      TokenSequenceMatcher nounMatcher = noun_phrase.matcher(tokens);
+      String firstNoun = nounMatcher.find() ? nounMatcher.group().toLowerCase() : null;
+
+      for (CoreMap sentence : sentences) {
+        if (iter != null) {
+          String id = (iter.hasNext() ? iter.next() : "?.?.?.?.?.?.?");
+          System.out.print(id);
+          System.out.print("\t");
+        }
+
+        tokens = sentence.get(CoreAnnotations.TokensAnnotation.class);
+        String gloss = sentence.get(CoreAnnotations.TextAnnotation.class).toLowerCase();
+        personMatcher = person.matcher(tokens);
+        String localPerson = personMatcher.find() ? personMatcher.group() : firstPerson;
+        pluralMatcher = plural.matcher(tokens);
+        String localPlural = pluralMatcher.find() ? pluralMatcher.group().toLowerCase() : null;
+        nounMatcher = noun_phrase.matcher(tokens);
+        String localNoun = nounMatcher.find() ? nounMatcher.group().toLowerCase() : null;
+
+        boolean sentenceStart = true;
+//        System.err.println(gloss);
+//        System.err.println("  firstPerson: " + firstPerson);
+//        System.err.println("  firstPlural: " + firstPlural);
+//        System.err.println("  firstNoun: " + firstNoun);
+//        System.err.println("  localPerson: " + localPerson);
+//        System.err.println("  localPlural: " + localPlural);
+//        System.err.println("  localNoun: " + localNoun);
+        for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+          switch (token.word().toLowerCase()) {
+            case "he":
+              if (localPerson != null && gloss.indexOf("he") > gloss.indexOf(localPerson.toLowerCase())) {
+                token.setWord(localPerson);
+              } else {
+                token.setWord(firstPerson == null ? token.word() : firstPerson);
+              }
+              break;
+            case "she":
+              if (localPerson != null && gloss.indexOf("she") > gloss.indexOf(localPerson.toLowerCase())) {
+                token.setWord(localPerson);
+              } else {
+                token.setWord(firstPerson == null ? token.word() : firstPerson);
+              }
+              break;
+            case "they":
+              if (localPerson != null && gloss.indexOf("they") > gloss.indexOf(localPerson.toLowerCase())) {
+                token.setWord(localPerson);
+              } else if (firstPerson != null) {
+                token.setWord(firstPerson);
+              } else if (localPlural != null && gloss.indexOf("they") > gloss.indexOf(localPlural)) {
+                  token.setWord(localPlural);
+              } else {
+                token.setWord(firstPlural == null ? token.word() : firstPlural);
+              }
+              break;
+            case "it":
+              if (!gloss.contains(" that ") && !gloss.contains(" because ") && !gloss.contains(" so ")) {
+                if (localNoun != null && gloss.indexOf("it") < gloss.indexOf(localNoun)) {
+                  token.setWord(localNoun);
+                } else {
+                  token.setWord(firstNoun == null ? token.word() : token.word());
+                }
+              }
+              break;
+          }
+          System.out.print(token.word());
+          System.out.print(" ");
+          sentenceStart = false;
+        }
+        System.out.println();
+      }
+      System.out.flush();
+
+      numDocsAnnotated += 1;
+      if (numDocsAnnotated % 1000 == 0) {
+        System.err.println("  Annotated " + (numDocsAnnotated / 1000) + "k documents: " + Redwood.formatTimeDifference(System.currentTimeMillis() - startTime));
+      }
+    }
+
+    System.out.flush();
+    System.err.println("DONE: " + Redwood.formatTimeDifference(System.currentTimeMillis() - startTime));
+  }
+
+
+  /*
   public static void main(String[] args) throws IOException {
     long startTime = System.currentTimeMillis();
     StanfordCoreNLP pipeline = new StanfordCoreNLP(new Properties() {{
@@ -74,7 +243,6 @@ public class ExpandCoreference {
 
 
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder text = new StringBuilder();
     Document currentDocument = new Document(new StringBuilder(), new ArrayList<>());
     String currentDocId = null;
     List<Document> documents = new ArrayList<>();
@@ -111,22 +279,11 @@ public class ExpandCoreference {
     System.err.println("Annotating...");
     int numDocsAnnotated = 0;
     for (Document doc : documents) {
-      doc.ann = new Annotation(doc.text.toString());
+      doc.ann = new Annotation(doc.text.toString().trim());
       pipeline.annotate(doc.ann);
-      numDocsAnnotated += 1;
-      if (numDocsAnnotated % 1000 == 0) {
-        System.err.println("  Annotated " + (numDocsAnnotated / 1000) + "k documents: " + Redwood.formatTimeDifference(System.currentTimeMillis() - startTime));
-      }
-    }
-    System.err.println("Annotation DONE: " + Redwood.formatTimeDifference(System.currentTimeMillis() - startTime));
-//    Annotation doc = new Annotation("George Bush went to China where he visited its prime minister.");
 
-    System.err.println("Writing output...");
-    for (Document document : documents) {
-      Annotation doc = document.ann;
-      List<String> ids = document.ids;
-      Map<Integer, CorefChain> corefChains = doc.get(CorefCoreAnnotations.CorefChainAnnotation.class);
-      List<CoreMap> sentences = doc.get(CoreAnnotations.SentencesAnnotation.class);
+      Map<Integer, CorefChain> corefChains = doc.ann.get(CorefCoreAnnotations.CorefChainAnnotation.class);
+      List<CoreMap> sentences = doc.ann.get(CoreAnnotations.SentencesAnnotation.class);
       for (CorefChain chain : corefChains.values()) {
         CorefChain.CorefMention canonical = chain.getRepresentativeMention();
         for (CorefChain.CorefMention mention : chain.getMentionsInTextualOrder()) {
@@ -139,10 +296,11 @@ public class ExpandCoreference {
         }
       }
 
-      Iterator<String> iter = ids.isEmpty() ? null : ids.iterator();
+      Iterator<String> iter = doc.ids.isEmpty() ? null : doc.ids.iterator();
       for (CoreMap sentence : sentences) {
         if (iter != null) {
-          System.out.print(iter.next());
+          String id = (iter.hasNext() ? iter.next() : "?.?.?.?.?.?.?");
+          System.out.print(id);
           System.out.print("\t");
         }
         for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
@@ -151,7 +309,16 @@ public class ExpandCoreference {
         }
         System.out.println();
       }
+      System.out.flush();
+
+      numDocsAnnotated += 1;
+      if (numDocsAnnotated % 1000 == 0) {
+        System.err.println("  Annotated " + (numDocsAnnotated / 1000) + "k documents: " + Redwood.formatTimeDifference(System.currentTimeMillis() - startTime));
+      }
     }
+    System.err.println("Annotation DONE: " + Redwood.formatTimeDifference(System.currentTimeMillis() - startTime));
+
     System.err.println("DONE: " + Redwood.formatTimeDifference(System.currentTimeMillis() - startTime));
   }
+  */
 }
