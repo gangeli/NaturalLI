246f99b577c25af377558ef0af90354a0b4ac8c8
Gabor Angeli <angeli@stanford.edu>
2015-05-28 20:53:02 -0700

    Misc writing

--- src/edu/stanford/nlp/naturalli/entail/NaturalLIClassifier.java
+++ src/edu/stanford/nlp/naturalli/entail/NaturalLIClassifier.java
@@ -228,7 +228,7 @@ public class NaturalLIClassifier implements EntailmentClassifier {
 
       });
       // Gobble naturalli.stderr to real stderr
-      Writer errWriter = new BufferedWriter(new FileWriter(new File("/home/gabor/workspace/naturalli/tmp/debug.txt")));
+      Writer errWriter = new BufferedWriter(new FileWriter(new File("/dev/null")));
 //      Writer errWriter = new OutputStreamWriter(System.err);
       StreamGobbler errGobbler = new StreamGobbler(searcher.getErrorStream(), errWriter);
       errGobbler.start();
