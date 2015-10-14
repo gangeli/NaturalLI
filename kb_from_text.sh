a2bf537691999c815a3a8f37688145eb287b7938
Gabor Angeli <angeli@stanford.edu>
2015-10-14 15:11:18 -0700

    Update the kb_from_text.sh helper

--- kb_from_text.sh
+++ kb_from_text.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
 
-make src/naturalli_client.jar
+make src/naturalli_preprocess.jar
 
 java -Dwordnet.database.dir=etc/WordNet-3.1/dict \
-  -cp $CLASSPATH:src/naturalli_client.jar \
-  edu.stanford.nlp.naturalli.Preprocess |\
-  src/naturalli_mkkb
+  -cp stanford-corenlp-3.5.3.jar:stanford-corenlp-models-current.jar:lib/jaws.jar:src/naturalli_preprocess.jar \
+  edu.stanford.nlp.naturalli.ProcessPremise |\
+  src/hash_tree  |\
+  src/write_kb $1
